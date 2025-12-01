2c0c015ffcfdaef564d6db5c701c8bb06e6ff0a2
Burundug <iburundugi@gmail.com>
2025-12-01 12:36:05 +0300

    add update

--- src/Scaffold/BasicScaffoldBuilder.php
+++ src/Scaffold/BasicScaffoldBuilder.php
@@ -15,12 +15,26 @@
             $configPath = $this->base . '/config.php';
             $existingConfig = $this->files->exists($configPath) ? $this->files->get($configPath) : null;
 
-            $this->files->copyDirectory($stubs, $this->base);
-
             $docsDir = trim($_ENV['DOCS_DIR'] ?? getenv('DOCS_DIR') ?? 'docs', '/\\');
             $targetDocs = $this->base . '/source/' . $docsDir;
             $stubDocs = $stubs . '/source/' . $docsDir;
-            if ($this->files->isDirectory($stubDocs) && ! $this->files->isDirectory($targetDocs)) {
+
+            $preserveDocs = $this->files->isDirectory($targetDocs);
+            $docsBackup = null;
+
+            if ($preserveDocs) {
+                $docsBackup = $this->base . '/source/' . $docsDir . '_backup_' . uniqid();
+                $this->files->move($targetDocs, $docsBackup);
+            }
+
+            $this->files->copyDirectory($stubs, $this->base);
+
+            if ($preserveDocs) {
+                if ($this->files->isDirectory($targetDocs)) {
+                    $this->files->deleteDirectory($targetDocs);
+                }
+                $this->files->move($docsBackup, $targetDocs);
+            } elseif ($this->files->isDirectory($stubDocs) && ! $this->files->isDirectory($targetDocs)) {
                 $this->files->copyDirectory($stubDocs, $targetDocs);
             }
 

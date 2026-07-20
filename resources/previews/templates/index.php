4fa4bbf7c0b6a0d59205fc8c58639f5709447a5b
Rim Zabarov <rim@zabarov.com>
2026-07-20 18:14:57 +0300

    fix: restore preview action contrast

--- resources/previews/templates/index.php
+++ resources/previews/templates/index.php
@@ -33,7 +33,7 @@
 <?php } ?>
                     </div>
 <?php if ($item->previewUrl !== null) { ?>
-                    <a class="sf-button sf-button--default sf-button--primary sf-button--size-1 radius-default decoration-none" href="<?= $item->previewUrl ?>">Открыть preview</a>
+                    <a class="sf-button sf-button--default sf-button--primary sf-button--size-1 bg-primary color-on-primary p-1/2 line-none radius-default inline-flex items-center content-main-center decoration-none" href="<?= $item->previewUrl ?>"><span class="sf-button-text-container">Открыть preview</span></a>
 <?php } else { ?>
                     <span class="surface-container border border-outline-variant radius-1 p-1">Только legacy</span>
 <?php } ?>
